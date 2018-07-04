f460d1b3fa15d41987b08c43fff66f22cab10915
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-07-04 18:35:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,12 +4,15 @@
 class Application
 
   def call(env)
+
     resp = Rack::Response.new
     req  = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      binding.pry
-      true
+      item_requested = req.path.split("/items/").last
+      item = @@items.find{|i| i.name = item_requested}
+      resp.write item.price
+      
     else
       resp.write "Route not found"
       resp.status = 404
