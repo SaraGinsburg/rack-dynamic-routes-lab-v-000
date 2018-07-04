d18c3b13ef514046f791a0f871db3221d4968f77
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-07-04 18:19:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,2 +1,10 @@
 # Your application should only accept the /items/<ITEM NAME> route.
 # Everything else should 404
+
+class application
+
+def call(env)
+  resp = Rack::Response.new
+  req  = Rack::Request.new(env)
+end
+end
