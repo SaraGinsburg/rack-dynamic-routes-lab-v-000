745d99640028b49db1fb96d80b7fa0358e5c9236
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-07-04 18:25:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,17 +3,18 @@
 
 class Application
 
-def call(env)
-  resp = Rack::Response.new
-  req  = Rack::Request.new(env)
+  def call(env)
+    resp = Rack::Response.new
+    req  = Rack::Request.new(env)
 
-  if req.path.match(/items/)
-    true
-  else
-    resp.write "Route not found"
-    resp.status = 404
+    if req.path.match(/items/)
+      binding.pry
+      true
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
   end
-
-  resp.finish
-end
 end
